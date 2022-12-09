c5013d95dacf645fd65d333cceab80d2f75ca373
MarcioGodoyLevada <marcio.levada@outlook.com>
2022-12-09 00:17:03 -0300

    adjust

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,6 @@ import { Router } from 'express'
 import { UserController } from './api/controllers/userController'
 import { authMiddleware } from './middlewares/authMiddleware'
 
-import { FriendsController } from './api/controllers/friendsController'
-
 const routes = Router()
 
 routes.post('/user', new UserController().create)
@@ -12,7 +10,5 @@ routes.post('/login', new UserController().login)
 routes.use(authMiddleware)
 
 routes.get('/profile', new UserController().getProfile)
-
-routes.get('/list-friends', new FriendsController().getFriends)
 
 export default routes
